6c38b9b32dac44d702c1220983d00c67f144000a
eckdev <eyupcankayadarcin@hotmail.com>
2024-10-24 17:49:59 +0300

    update favico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link
+          rel="shortcut icon"
+          href="/favicon.ico"
+          type="image/x-icon"
+        ></link>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1338120620743783"
